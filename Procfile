db5140dcfde47dd92783fc835507fda7094d638f
Emily Esten <estenemily@gmail.com>
2022-12-08 12:34:59 -0500

    created person explorer viz

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: bokeh serve --port=$PORT --allow-websocket-origin=snl-coefficient.herokuapp.com --address=0.0.0.0 --use-xheaders main.py
+web: bokeh serve --port=$PORT --allow-websocket-origin=snl-alum-coefficient.herokuapp.com --address=0.0.0.0 --use-xheaders main.py
--- main.py
+++ main.py
@@ -12,48 +12,46 @@ from bokeh.plotting import figure
 path = os.getcwd()
 print(path)
 #path = path.rsplit('/')[0]
-movies = pd.read_csv(f"{path}/data/full_data_snl_movies_coefficient.csv")
+movies = pd.read_csv(f"{path}/data/performers_with_coefficient.csv")
+print(movies)
+
 snl_cast_crew = pd.read_csv(f"{path}/data/snl_cast_crew.csv")
+seasons_list = ["All","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36","37","38","39","40","41","42","43","44","45","46","47","48","49","50","51","52"]
 snl_alums_list = snl_cast_crew['person'].to_list()
 snl_alums_list.sort()
 snl_alums_list.append("All")
-#snl_alums_list.sort()
-snl_media = open(join(dirname(__file__), f"{path}/data/snl_associated_media.txt")).read().split()
+snl_alums_list.sort()
+#snl_media = open(join(dirname(__file__), f"{path}/data/snl_associated_media.txt")).read().split()
 movies["color"] = "grey"
-movies.loc[movies.imdb_link.isin(snl_media), "color"] = "purple"
+#movies.loc[movies.imdb_link.isin(snl_media), "color"] = "purple"
 movies["alpha"] = 1
 
 axis_map = {
-    "SNL Coefficient": "movie_coefficient",
+    "Person Coefficient": "coefficient",
     "Year (Start)": "year_start",
 }
 
 desc = Div(text=open(join(dirname(__file__), "description.html")).read(), sizing_mode="stretch_width")
 
 # Create Input controls
-min_coefficient = Slider(title="Minimum coefficient", value=0, start=0, end=20, step=.01) 
-cast_count = Slider(title="Minimum number of SNL alums", value=2, start=2, end=100, step=1)
-min_year = Slider(title="Year released", start=1947, end=2023, value=1970, step=1)
-max_year = Slider(title="End Year released", start=1947, end=2023, value=2022, step=1)
-genre = Select(title="Genre", value="All",
-               options=open(join(dirname(__file__), f"{path}/data/genres.txt")).read().split())
-medium = Select(title="Medium", value="All",
-               options=[i.strip() for i in open(f"{path}/data/media_types.txt").readlines()]) 
+min_coefficient = Slider(title="Minimum coefficient", value=0, start=0, end=1, step=.001)
+season = Select(title="Season", value="All", options=seasons_list)
 snl_alumni = Select(title="SNL Alum", value="All", options=snl_alums_list)
-x_axis = Select(title="X Axis", options=sorted(axis_map.keys()), value="Year (Start)")
-y_axis = Select(title="Y Axis", options=sorted(axis_map.keys()), value="SNL Coefficient")
-specifics = Select(title="Curated Lists", value="None",options=['None','SNL-Related Media','Non-SNL-Related Media','SNL Feature Films', 'Happy Madison Productions', 'NBC']) 
-title_input = TextInput(value="", title="Search for Media:")
+x_axis = Select(title="X Axis", options=["Year (Start)"], value="Year (Start)")
+y_axis = Select(title="Y Axis", options=["Person Coefficient"], value="Person Coefficient")
+roles = Select(title='Roles', value="All", options=["All","Writers","Performers"])
+specifics = Select(title="Curated Lists", value="None",options=['None','Five-Timers Club','Head Writers','Weekend Update', 'Best Of Compilations', 'Alumni Hosts'])
+lorne =  Select(title="Include Lorne?", options=["Yes","No"], value="No")
+title_input = TextInput(value="", title="Search for a Person:")
 
 source = ColumnDataSource(data=dict(x=[], y=[], color=[], title=[], year=[], alpha=[]))
 
 TOOLTIPS=[
-    ("Title", "@title"),
+    ("Alum", "@person"),
     ("Year (Start)", "@year_start"),
-    ("Genre", "@genres"),
-    ("Medium", "@medium"),
-    ("Cast Count", "@cast_count"),
-    ("Coefficient", "@movie_coefficient")
+    ("Year (End)", "@year_end"),
+    ("Number of Seasons", "@num_seasons_total"),
+    ("Coefficient", "@coefficient")
 ]
 
 p = figure(height=600, width=700, title="", toolbar_location=None, tooltips=TOOLTIPS, sizing_mode="scale_both")
@@ -61,39 +59,42 @@ p.circle(x="x", y="y", source=source, size=7, color="color", line_color=None, fi
 
 
 def select_movies():
-    genre_val = genre.value
-    medium_val = medium.value
     snl_alumni_val = snl_alumni.value
     specifics_val = specifics.value
-    title_input_val = title_input.value 
-    selected = movies[
-        (movies.year_start >= min_year.value) &
-        (movies.year_end <= max_year.value) &
-        (movies.movie_coefficient >= min_coefficient.value) &
-        (movies.cast_count >= cast_count.value)
-        ]
-    print("len selected")
-    if (genre_val != "All"):
-        selected = selected[selected.genres.str.contains(genre_val)==True]
-    if (medium_val != "All"):
-        selected = selected[selected.medium.str.contains(medium_val)==True]
+    title_input_val = title_input.value
+    roles_val = roles.value
+    season_val = season.value
+    lorne_val = lorne.value
+    selected = movies[(movies.coefficient >= min_coefficient.value)]
+    if (lorne_val=="No"):
+        selected = selected[selected.person.str.contains("Lorne Michaels")==False]
     if (snl_alumni_val != "All"):
-        selected = selected[selected.snl_alums.str.contains(snl_alumni_val)==True]
+        selected = selected[selected.person.str.contains(snl_alumni_val)==True]
+    if (roles_val != "All"):
+        if roles_val =="Writers":
+            selected = selected[selected.num_seasons_writer>=1]
+        else:
+            selected = selected[selected.num_seasons_actor>=1]
+    if (season_val != "All"):
+        seasons_dict = {"1":1975,"2":1976,"3":1977,"4":1978,"5":1979,"6":1980,"7":1981,"8":1982,"9":1983,"10":1984,"11":1985,"12":1986,"13":1987,"14":1988,"15":1989,"16":1990,"17":1991,"18":1992,"19":1993,"20":1994,"21":1995,"22":1996,"23":1997,"24":1998,"25":1999,"26":2000,"27":2001,"28":2002,"29":2003,"30":2004,"31":2005,"32":2006,"33":2007,"34":2008,"35":2009,"36":2010,"37":2011,"38":2012,"39":2013,"40":2014,"41":2015,"42":2016,"43":2017,"44":2018,"45":2019,"46":2020,"47":2021,"48":2022,"49":2023,"50":2024,"51":2025,"52":2026}
+        season_start = seasons_dict[season_val]
+        season_end = seasons_dict[season_val] + 1
+        selected = selected[selected['year_start']<=season_start]
+        selected = selected[selected['year_end']>=season_end]
     if (specifics_val != "None"):
-       if specifics_val == "SNL Feature Films":         
-           feature_films = [i.strip() for i in open(f"{path}/data/snl_feature_films.txt").readlines()]
-           print(feature_films)
-           selected = selected[selected['imdb_link'].isin(feature_films)]
-       elif specifics_val == 'SNL-Related Media':
-            selected = selected[selected['imdb_link'].isin(snl_media)]
-       elif specifics_val == 'Non-SNL-Related Media': 
-            selected = selected[~selected['imdb_link'].isin(snl_media)]
+       if specifics_val == "Five-Timers Club":
+           selected = selected[selected['num_episodes_hosted']>=5]
+       elif specifics_val == 'Head Writers':
+            selected = selected[selected['num_seasons_headwriter']>=1]
+       elif specifics_val == 'Weekend Update':
+            selected = selected[selected['num_seasons_weekend_update']>=1]
+       elif specifics_val == 'Best of Compilations':
+            selected = selected[selected['best_of']!=0]
        else:
-           selected = selected[selected.production_companies.str.contains(specifics_val)==True]
+           selected = selected[selected['num_episodes_hosted']>=1]
     if (title_input_val != ""):
         selected = selected[selected.title.str.contains(title_input_val)==True]
     return selected
-
 
 def update():
     df = select_movies()
@@ -101,23 +102,20 @@ def update():
     y_name = axis_map[y_axis.value]
     p.xaxis.axis_label = x_axis.value
     p.yaxis.axis_label = y_axis.value
-    p.title.text = "%d movies selected" % len(df)
+    p.title.text = "%d alumnni selected" % len(df)
     source.data = dict(
         x=df[x_name],
         y=df[y_name],
         color=df["color"],
-        cast_count=df["cast_count"],
-        title=df["title"],
+        person=df['person'],
         year_start=df["year_start"],
+        year_end=df["year_end"],
         alpha=df["alpha"],
-        movie_coefficient=df["movie_coefficient"],
-        medium=df["medium"],
-        genres=df["genres"],
-        imdb_link=df["imdb_link"],
-        production_companies=df["production_companies"]
+        coefficient=df["coefficient"],
+        num_seasons_total=df['num_seasons_total'],
     )
 
-controls = [genre, medium, cast_count, min_year, snl_alumni, min_coefficient, x_axis, y_axis, specifics, title_input]
+controls = [season, roles, lorne, snl_alumni, min_coefficient, x_axis, y_axis, specifics, title_input]
 for control in controls:
     control.on_change('value', lambda attr, old, new: update())
 
@@ -128,4 +126,4 @@ l = column(desc, row(inputs, p), sizing_mode="scale_both")
 update()  # initial load of the data
 
 curdoc().add_root(l)
-curdoc().title = "Movies"
+curdoc().title = "SNL Alumni"
